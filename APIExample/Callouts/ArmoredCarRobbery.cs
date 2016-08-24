e1dfe4edf454e6c51a3a38454c7cbc7c89499e75
Goigle <goigle@codethree.net>
2016-08-24 10:56:38 -0400

    Implement switching to SWAT team

--- APIExample/Callouts/ArmoredCarRobbery.cs
+++ APIExample/Callouts/ArmoredCarRobbery.cs
@@ -7,7 +7,7 @@ using System;
 using System.Threading;
 using System.Diagnostics;
 using System.Drawing;
-
+using FederalCallouts.Tools;
 //Our namespace (aka folder) where we keep our callout classes.
 namespace FederalCallouts.Callouts
 {
@@ -30,8 +30,12 @@ namespace FederalCallouts.Callouts
         private bool robbersEnRoute = true;
         private uint lastLocationUpdate = 0;
         private CarRobberyState state = CarRobberyState.EnRoute;
-
-
+        private bool swatDispatched = false;
+        private bool playerPromptedToSwitch = false;
+        private bool playerSwitchedChars = false;
+        private bool playerPromptedToEnd = false;
+        private Vehicle swatVehicle;
+        private CharacterManager charMan = new CharacterManager();
         /// <summary>
         /// OnBeforeCalloutDisplayed is where we create a blip for the user to see where the pursuit is happening, we initiliaize any variables above and set
         /// the callout message and position for the API to display
@@ -40,7 +44,7 @@ namespace FederalCallouts.Callouts
         public override bool OnBeforeCalloutDisplayed()
         {
             RelationshipGroup r = new RelationshipGroup("ROBBERS");
-            
+
             SpawnPoint = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(500f));
             GameFiber.StartNew((() =>
             {
@@ -69,11 +73,12 @@ namespace FederalCallouts.Callouts
                 attacker4.WarpIntoVehicle(robberyVan, (int)robberyVan.GetFreeSeatIndex());
                 attacker1.Inventory.GiveNewWeapon(new WeaponAsset("WEAPON_ASSAULTRIFLE"), 500, true);
                 robberyVan.IsPersistent = true;
-                robberyVan.Invincible = true;
+                robberyVan.IsInvincible = true;
 
-            }));
+            }), "FC Armored Car Robbery Init");
             guard1 = new Ped("s_m_m_armoured_01", SpawnPoint, 0.0f);
             guard2 = new Ped("s_m_m_armoured_02", SpawnPoint, 0.0f);
+            GameFiber.Sleep(6);
             guard1.CanAttackFriendlies = false;
             guard2.CanAttackFriendlies = false;
             guard1.Armor = 100;
@@ -189,7 +194,7 @@ namespace FederalCallouts.Callouts
                             Functions.PlayScannerAudioUsingPosition("ATTENTION_ALL_SWAT_UNITS ASSISTANCE_REQUIRED IN_OR_ON_POSITION", securicar.Position);
                             //IF YOU CHANGE THIS CHANGE THE OTHER ONE
                             Vehicle swatGranger = Functions.RequestBackup(securicar.Position, EBackupResponseType.Code3, EBackupUnitType.NooseTeam);
-                            swatGranger.Position = Extensions.FindCloseSpawn(securicar.Position, 150f, 175f);
+                            //swatGranger.Position = Extensions.FindCloseSpawn(securicar.Position, 150f, 175f);
                             //Gathered from http://ragepluginhook.net/PedModels.aspx?modelHash=2374966032 , this will make them have FIB SWAT textures
                             GameFiber.Wait(2000);
                             foreach (Ped p in swatGranger.Passengers)
@@ -201,6 +206,7 @@ namespace FederalCallouts.Callouts
                                 swatGranger.Driver.SetVariation(10, 0, 1);
                             //Making the guards and the SWAT friendly with each other
                             vBlip = swatGranger.AttachBlip();
+                            swatVehicle = swatGranger;
                             vBlip.Color = Color.LightBlue;
                             Game.SetRelationshipBetweenRelationshipGroups(swatGranger.Driver.RelationshipGroup, guard1.RelationshipGroup, Relationship.Companion);
                             Game.SetRelationshipBetweenRelationshipGroups(guard1.RelationshipGroup, swatGranger.Driver.RelationshipGroup, Relationship.Companion);
@@ -211,14 +217,64 @@ namespace FederalCallouts.Callouts
                             Functions.PlayScannerAudioUsingPosition("ATTENTION_ALL_SWAT_UNITS ASSISTANCE_REQUIRED IN_OR_ON_POSITION", securicar.Position);
                             //IF YOU CHANGE THIS CHANGE THE OTHER ONE
                             Vehicle swatGranger = Functions.RequestBackup(securicar.Position, EBackupResponseType.Code3, EBackupUnitType.NooseTeam);
-                            swatGranger.Position = Extensions.FindCloseSpawn(securicar.Position, 150f, 175f);
+                            //swatGranger.Position = Extensions.FindCloseSpawn(securicar.Position, 150f, 175f);
                             GameFiber.Wait(1000);
+                            swatVehicle = swatGranger;
                             vBlip = swatGranger.AttachBlip();
                             vBlip.Color = Color.LightBlue;
                             Game.SetRelationshipBetweenRelationshipGroups(swatGranger.Driver.RelationshipGroup, guard1.RelationshipGroup, Relationship.Companion);
                             Game.SetRelationshipBetweenRelationshipGroups(guard1.RelationshipGroup, swatGranger.Driver.RelationshipGroup, Relationship.Companion);
                         }
-                    }));
+                        GameFiber.StartNew((() =>
+                        {
+                            GameFiber.Sleep(1500);
+                            swatVehicle.MakePersistent();
+                            if (swatVehicle.PassengerCount == 0)
+                            {
+                                Game.LogTrivial("[FC] Recreating vehicle crew");
+                                for (int i = 0; i < 3; i++)
+                                {
+                                    Ped p = new Ped("s_m_y_swat_01", swatVehicle.Position.Around(10), 0);
+                                    for (int h = 0; h < 50; h++)
+                                    {
+                                        if (p)
+                                            break;
+                                        else
+                                            GameFiber.Wait(5);
+                                    }
+                                    if (p)
+                                    {
+                                        Functions.SetPedAsCop(p);
+                                        p.Armor = 100;
+                                        p.Inventory.GiveNewWeapon(new WeaponAsset("WEAPON_CARBINERIFLE"), 500, true);
+                                        p.WarpIntoVehicle(swatVehicle, swatVehicle.GetFreePassengerSeatIndex() ?? i);
+                                        p.Tasks.Clear();
+                                    }
+                                }
+                            }
+                            if (!swatVehicle.Driver)
+                            {
+                                Game.LogTrivial("[FC] Recreating vehicle driver");
+                                Ped p = new Ped("s_m_y_swat_01", swatVehicle.Position.Around(10), 0);
+                                for (int h = 0; h < 50; h++)
+                                {
+                                    if (p)
+                                        break;
+                                    else
+                                        GameFiber.Wait(5);
+                                }
+                                if (p)
+                                {
+                                    Functions.SetPedAsCop(p);
+                                    p.Armor = 100;
+                                    p.Inventory.GiveNewWeapon(new WeaponAsset("WEAPON_CARBINERIFLE"), 500, true);
+                                    p.WarpIntoVehicle(swatVehicle, swatVehicle.GetFreeSeatIndex() ?? -1);
+                                    p.Tasks.DriveToPosition(securicar.Position, 100, VehicleDrivingFlags.Emergency);
+                                }
+                            }
+                            swatDispatched = true;
+                        }), "FC Fiber 2");
+                    }), "FC Armored Car Robbery Backup");
 
                 Game.LogTrivialDebug("[FC] Attaching attacker blips");
                 aBlip1 = attacker1.AttachBlip();
@@ -249,10 +305,8 @@ namespace FederalCallouts.Callouts
                     {
                         VehicleDoor r1 = securicar.Doors[2];
                         VehicleDoor r2 = securicar.Doors[3];
-                        r1.IsFullyOpen = true;
-                        r2.IsFullyOpen = true;
-                        //r1.Remove();
-                        //r2.Remove();
+                        r1.BreakOff();
+                        r2.BreakOff();
                     }
                     Game.LogTrivialDebug("[FC] Guards are exiting the vehicle");
                     if (guard1.Exists() && guard1.IsInAnyVehicle(false)) { guard1.Tasks.LeaveVehicle(LeaveVehicleFlags.LeaveDoorOpen); }
@@ -274,21 +328,62 @@ namespace FederalCallouts.Callouts
                 GameFiber.StartNew((() =>
                 {
                     Dispatch.PlayerSay("Dispatch, I've located the armored car.");
-                    if (!robbersEnRoute)
-                    {
-                        GameFiber.Wait(1000);
-                        Dispatch.PlayerSay("I have eyes on the robbers!");
-                    }
                     GameFiber.Wait(2000);
                     Dispatch.Copy();
                 }));
             }
-
+            if (state == CarRobberyState.EnRoute & swatDispatched)
+            {
+                if (!playerPromptedToSwitch)
+                {
+                    Game.DisplayHelp("Press ~g~" + Settings.StartKey.ToString() + "~w~ now to switch to the SWAT team.");
+                    playerPromptedToSwitch = true;
+                }
+                if (Game.IsKeyDown(Settings.StartKey))
+                {
+                    if (!swatVehicle.Driver.Exists())
+                    {
+                        Game.DisplayNotification("SWAT driver was cleaned up by GTA, could not switch.");
+                    }
+                    else
+                    {
+                        playerSwitchedChars = true;
+                        GameFiber.StartNew((() =>
+                        {
+                            if (swatVehicle.PassengerCount > 0)
+                                foreach (Ped p in swatVehicle.Passengers)
+                                    p.Delete();
+                            charMan.SwitchToPed(swatVehicle.Driver);
+                            GameFiber.Sleep(4000);
+                            Game.LogTrivial("[FC] Recreating vehicle crew");
+                            for (int i = 0; i < 3; i++)
+                            {
+                                Ped p = new Ped("s_m_y_swat_01", swatVehicle.Position.Around(10), 0);
+                                for (int h = 0; h < 50; h++)
+                                {
+                                    if (p)
+                                        break;
+                                    else
+                                        GameFiber.Wait(5);
+                                }
+                                if (p)
+                                {
+                                    Functions.SetPedAsCop(p);
+                                    p.Armor = 100;
+                                    p.Inventory.GiveNewWeapon(new WeaponAsset("WEAPON_CARBINERIFLE"), 500, true);
+                                    p.WarpIntoVehicle(swatVehicle, swatVehicle.GetFreePassengerSeatIndex() ?? i);
+                                    p.Tasks.Clear();
+                                }
+                            }
+                        }), "Unnamed FC Fiber");
+                    }
+                }
+            }
             bool c1, c2, c3, c4;
-            if (attacker1.Exists()) { c1 = true; } else { c1 = false; }
-            if (attacker2.Exists()) { c2 = true; } else { c2 = false; }
-            if (attacker3.Exists()) { c3 = true; } else { c3 = false; }
-            if (attacker4.Exists()) { c4 = true; } else { c4 = false; }
+            if (attacker1) { c1 = true; } else { c1 = false; }
+            if (attacker2) { c2 = true; } else { c2 = false; }
+            if (attacker3) { c3 = true; } else { c3 = false; }
+            if (attacker4) { c4 = true; } else { c4 = false; }
             if (c1 && Functions.IsPedArrested(attacker1) || !attacker1.IsAlive) { c1 = false; }
             if (c2 && Functions.IsPedArrested(attacker2) || !attacker2.IsAlive) { c2 = false; }
             if (c3 && Functions.IsPedArrested(attacker3) || !attacker3.IsAlive) { c3 = false; }
@@ -296,14 +391,26 @@ namespace FederalCallouts.Callouts
             //False means they are dead, arrested or do not exist so it is safe to end the callout
             if (!c1 && !c2 && !c3 && !c4)
             {
-                GameFiber.StartNew((() =>
+                if (playerSwitchedChars)
                 {
-                    GameFiber.Wait(1000);
-                    Dispatch.PlayerSay("Dispatch, the situation is ~g~code 4~w~.");
-                    GameFiber.Wait(2000);
-                    Dispatch.Copy();
-                }));
-                End();
+                    if (!playerPromptedToEnd)
+                    {
+                        Game.DisplayHelp("Press ~g~" + Settings.StartKey + "~w~ to end the callout and switch to your character.");
+                        Game.DisplaySubtitle("Press ~g~" + Settings.StartKey + "~w~ to end the callout and switch to your character.", 30);
+                        playerPromptedToEnd = true;
+                    }
+                }
+                if(!playerSwitchedChars | Game.IsKeyDown(Settings.StartKey))
+                {
+                    GameFiber.StartNew((() =>
+                    {
+                        GameFiber.Wait(1000);
+                        Dispatch.PlayerSay("Dispatch, the situation is ~g~code 4~w~.");
+                        GameFiber.Wait(2000);
+                        Dispatch.Copy();
+                    }));
+                    End();
+                }
             }
             base.Process();
         }
@@ -314,21 +421,23 @@ namespace FederalCallouts.Callouts
         /// </summary>
         public override void End()
         {
-            if (guard1.Exists()) { guard1.Dismiss(); }
-            if (guard2.Exists()) { guard2.Dismiss(); }
-            if (robberyVan.Exists()) { robberyVan.Dismiss(); }
-            if (securicar.Exists()) { securicar.Dismiss(); }
-            if (gBlip1.Exists()) { gBlip1.Delete(); }
-            if (gBlip2.Exists()) { gBlip2.Delete(); }
-            if (aBlip1.Exists()) { aBlip1.Delete(); }
-            if (aBlip2.Exists()) { aBlip2.Delete(); }
-            if (aBlip3.Exists()) { aBlip3.Delete(); }
-            if (aBlip4.Exists()) { aBlip4.Delete(); }
-            if (vBlip.Exists()) { vBlip.Delete(); }
-            if (attacker1.Exists()) { attacker1.Dismiss(); }
-            if (attacker2.Exists()) { attacker2.Dismiss(); }
-            if (attacker3.Exists()) { attacker3.Dismiss(); }
-            if (attacker4.Exists()) { attacker4.Dismiss(); }
+            if (guard1) { guard1.Dismiss(); }
+            if (guard2) { guard2.Dismiss(); }
+            if (robberyVan) { robberyVan.Dismiss(); }
+            if (securicar) { securicar.Dismiss(); }
+            if (gBlip1) { gBlip1.Delete(); }
+            if (gBlip2) { gBlip2.Delete(); }
+            if (aBlip1) { aBlip1.Delete(); }
+            if (aBlip2) { aBlip2.Delete(); }
+            if (aBlip3) { aBlip3.Delete(); }
+            if (aBlip4) { aBlip4.Delete(); }
+            if (vBlip) { vBlip.Delete(); }
+            if (attacker1) { attacker1.Dismiss(); }
+            if (attacker2) { attacker2.Dismiss(); }
+            if (attacker3) { attacker3.Dismiss(); }
+            if (attacker4) { attacker4.Dismiss(); }
+            if (playerSwitchedChars)
+                charMan.SwitchToBackup();
             base.End();
         }
     }
